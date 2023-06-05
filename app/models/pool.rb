430d318cde540cf2de254a9cbf58ee910c2ab0e1
Shuqi Zheng <shuqizheng6@gmail.com>
2023-06-05 15:53:52 +0100

    Added devise to user model

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,3 +1,6 @@
 class Pool < ApplicationRecord
   belongs_to :user
+
+  validates :address, :price, presence: true
+  validates :size, inclusion: { in: ['small', 'medium', 'large'] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
+  root to: "pools#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :pools, only: [ :index, :new, :create, :show ] do
+    resources :bookings, only: [ :new, :create, :edit, :update ]
+  end
+  resources :bookings, only: :destroy
 end
