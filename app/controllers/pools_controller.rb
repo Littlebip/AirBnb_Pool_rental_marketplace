a68daff65f2100072a7f13043d3eeeadf63027e7
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-06-06 16:42:49 +0200

    Created show page with uncomplete styling. Added a bookings page

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,12 @@
 class PoolsController < ApplicationController
+  def show
+    set_pool
+    @booking = Booking.new
+  end
+
+  private
+
+  def set_pool
+    @pool = Pool.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :pools, only: [ :index, :new, :create, :show ] do
-    resources :bookings, only: [ :new, :create, :edit, :update ]
+    resources :bookings, only: [:index, :create, :edit, :update ]
   end
   resources :bookings, only: :destroy
 end
