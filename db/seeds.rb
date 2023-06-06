011729550483f2fa3e2198b90b73db9db9c1f81d
Ayub Farah <ayubfarahh@gmail.com>
2023-06-06 11:56:02 +0100

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,10 @@ Pool.destroy_all
 
 SIZES = ["small", "medium", "large"]
 
-# user = User.create(username:"bob", email: "bob@gmail.com", password:"1234567")
+user = User.create(username:"ayub", email: "ayub@gmail.com", password:"123456")
+user = User.create(username:"lucile", email: "lucile@gmail.com", password:"123456")
+user = User.create(username:"shuqi", email: "shuqi@gmail.com", password:"123456")
+user = User.create(username:"elisa", email: "elisa@gmail.com", password:"123456")
 15.times do
   address = Faker::Address.full_address
   size = SIZES.sample
