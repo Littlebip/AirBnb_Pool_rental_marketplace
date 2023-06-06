bb2fffb1917e604851a00ae91b7bb8474048a0a3
Ayub Farah <ayubfarahh@gmail.com>
2023-06-06 16:14:21 +0100

    created new pool page

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,22 @@
 class PoolsController < ApplicationController
+  def index
+    @pools = Pool.all
+  end
+
+  def new
+    @pool = Pool.new
+  end
+
+  def create
+    @pool = Pool.create(pool_params)
+    redirect_to pool_path(@pool)
+  end
+
+
+  private
+
+  def pool_params
+    params.require(:pool).permit(:address, :size, :price, :details, :images)
+  end
+
 end
