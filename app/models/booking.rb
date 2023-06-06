dbac2b6a1eb970dd05a5e915a30f154c537cffc7
Ayub Farah <ayubfarahh@gmail.com>
2023-06-06 11:52:42 +0100

    Added seeds/validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pool
 
-  validates :start_date, :end_date, :status, presence: true
+  validates :start_date, :end_date, presence: true
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :status, inclusion: { in: ['accepted', 'declined'] }
+
 end
