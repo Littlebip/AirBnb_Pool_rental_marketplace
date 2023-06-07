3eda2d5419785095603b226e8fe3a692e00605d8
Ayub Farah <ayubfarahh@gmail.com>
2023-06-07 10:27:02 +0100

    merged master

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -21,9 +21,9 @@ class PoolsController < ApplicationController
 
   def set_pool
     @pool = Pool.find(params[:id])
+  end
 
   def pool_params
     params.require(:pool).permit(:address, :size, :price, :details, :images)
   end
-
 end
