0c70dc0ff3e08b46c88fd76238f9536d2858ee79
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-06-08 09:07:07 +0200

    Created cloudinary links, still missing link on homepage, bugs on show carousel

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,6 +1,8 @@
 class Pool < ApplicationRecord
   belongs_to :user
 
+  has_many_attached :images
+
   validates :address, :price, presence: true
   validates :size, inclusion: { in: ['small', 'medium', 'large'] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,29 @@ Pool.destroy_all
 SIZES = ["small", "medium", "large"]
 
 puts "Creating users..."
-User.create(username: "ayub", email: "ayub@gmail.com", password: "123456")
-User.create(username: "lucile", email: "lucile@gmail.com", password: "123456")
-User.create(username: "shuqi", email: "shuqi@gmail.com", password: "123456")
-User.create(username: "elisa", email: "elisa@gmail.com", password: "123456")
+@user_ayub = User.create(username: "ayub", email: "ayub@gmail.com", password: "123456")
+@user_lucile = User.create(username: "lucile", email: "lucile@gmail.com", password: "123456")
+@user_shuqi = User.create(username: "shuqi", email: "shuqi@gmail.com", password: "123456")
+@user_elisa = User.create(username: "elisa", email: "elisa@gmail.com", password: "123456")
 
 puts "Creating pools..."
 15.times do
-  @title = "#{Faker::Color.color_name} pool with #{Faker::Marketing.buzzwords}"
+  @title = "#{Faker::Color.color_name.capitalize} pool with #{Faker::House.furniture}s"
   @address = Faker::Address.full_address
   @size = SIZES.sample
-  @price = rand(0..1_000_000)
+  @price = rand(70..250)
+  @details = "The water in the pool is blue as the sky and clean as the crystal. The cool blue water of the pool is refreshing mind and body."
+  @images = [url("https://source.unsplash.com/random/?pool,sun"), url("https://source.unsplash.com/random/?pool,cocktail"), url("https://source.unsplash.com/random/?pool")]
+  @user_id_array = [@user_ayub.id, @user_lucile.id, @user_shuqi.id, @user_elisa.id]
 
   pool = Pool.create!(
     title: @title,
     address: @address,
     size: @size,
     price: @price,
-    user_id: rand(1..4)
+    user_id: @user_id_array.sample,
+    details: @details,
+    images: @images
   )
 
   puts "Created pool #{pool.title}"
